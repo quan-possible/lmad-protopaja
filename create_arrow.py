035f862bb56491c58e7e014b204d8ea6f365be8d
trung170801 <55836090+trung170801@users.noreply.github.com>
2020-06-29 00:28:55 +0300

    Added turning-angle calculation

--- create_arrow.py
+++ create_arrow.py
@@ -33,9 +33,9 @@ def paint_path(image, road_val_range):
     image : The painted version of the image.
 
     """
-
+    font = cv2.FONT_HERSHEY_SIMPLEX
     height, width = int(image.shape[0]), int(image.shape[1]-1)
-
+    current_pos = height-1,width/2
     # This is the substitute for the road_val_range.
     # When the pixels where the roads appear is determined,
     # they are assigned to the first value of the tuple.
@@ -48,6 +48,7 @@ def paint_path(image, road_val_range):
     def find_road_top_bot(image):
         height, width = int(image.shape[0]), int(image.shape[1])
         try:
+            # Remove the bottom part of the image from the search by slicing until (height-height/10)
             itemindex = np.where(image[0:int(height-height/10),:] == reclassifying_val[0])
             return itemindex[0][0], itemindex[0][::-1][0]
         except:
@@ -79,12 +80,14 @@ def paint_path(image, road_val_range):
         gray[~cond(gray)] = reclassifying_val[1]     # Boolean Indexing
         return gray
 
-    # Confer https://scikit-image.org/docs/stable/api/skimage.draw.html#skimage.draw.line
-    def draw_line(x, y) -> np.ndarray:
-        rr, cc = draw.line(
-            int(x[0]), int(x[1]), int(y[0]), int(y[1]))
-        image[rr, cc] = 255
-
+    # From the given list of average points, calculate the angle between current_pos and its nearest point.
+    def get_turning_angle(points_list):
+        destination = points_list[-1]
+        bot_right = (height-height/10,width/2)
+        ang = math.degrees(math.atan2(destination[1]-current_pos[1], destination[0]-current_pos[0]) - \
+            math.atan2(bot_right[1]-current_pos[1], bot_right[0]-current_pos[0]))
+        # return str(ang + 360) if ang < 0 else str(ang)
+        return str(360+ang) if ang < -180 else str(ang)
 
     # Process the image. Refer to the process_image function for more details.
     processed_img = process_image(image, cond)
@@ -111,13 +114,25 @@ def paint_path(image, road_val_range):
 
         # Reduce the number of coordinates to smoothen the path.    
         avg_list = avg_list[::10]
+
+        # Add turning angle to the screen
+        turning_angle = get_turning_angle(avg_list)
+        text_position = int(height/10),int(width/10)
+        cv2.putText(image,turning_angle,text_position,font,1,(255,255,255),1,cv2.LINE_AA)
+
         # We also have to add our place in to the list .ie
         # the bottom middle point.
-        avg_list.append((height-1,width/2))
+        avg_list.append(current_pos)
         nice = zip(avg_list,avg_list[1:]) # Zip these to make pairs.
 
+        # Confer https://scikit-image.org/docs/stable/api/skimage.draw.html#skimage.draw.line
         for x, y in nice:
-            draw_line(x,y)
+            rr, cc = draw.line(
+                int(x[0]), int(x[1]), int(y[0]), int(y[1]))
+            image[rr, cc] = 255
+    else:        
+        cv2.putText(image,'No pavement detected',(height/2,width/2),font,3,(255,255,255),2, cv2.LINE_AA)
+        cv2
 
     return image
 
